9070ffc63dc87c8f64126f2ba8055427acefa0ad
Billy Halim <bkhalim@ucsd.edu>
2021-05-12 08:47:02 -0700

    test

--- server/app.js
+++ server/app.js
@@ -7,9 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/build/index.html'));
+// });
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
